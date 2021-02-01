96a706faf422a0ac246d693c584759ecf38aa9a9
Chip Kennedy <chip@docollectively.com>
2021-02-01 18:52:48 -0500

    Reset db config

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ module.exports = {
 	"database": process.env.MYSQL_DATABASE,
 	"port": process.env.MYSQL_PORT,
 	"host": process.env.MYSQL_HOST,
-	//"logging": false,
-	"logging": console.log,
+	"logging": false,
+	//"logging": console.log,
 	"dialect": "mysql"
 }
